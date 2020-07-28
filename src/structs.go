0a0e85a2218b333298a32234eb4b80b0addbbbd0
P33king <yunhao.linux@gmail.com>
2020-07-28 17:45:20 +0800

    new structs.go

---
+++ src/structs.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+type Strtext struct {
+	Z string
+	W string
+}
+
+func main() {
+	fmt.Println(Vertex{1, 2})
+	//	fmt.Println(Strtext{1, 2})
+}
