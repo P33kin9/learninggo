4b143a9de9fc82aed9884f4caf65c489b23a08b8
P33king <yunhao.linux@gmail.com>
2020-11-03 23:43:18 +0800

    modify add prefix http://

--- Tgopl/fetch/main.go
+++ Tgopl/fetch/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
+
 	//"io/ioutil"
 	"net/http"
 	"os"
@@ -10,6 +12,9 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
