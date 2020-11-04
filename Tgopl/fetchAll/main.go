d5d6aa59b88e7be04fdc89fed6567970eef383d8
P33king <yunhao.linux@gmail.com>
2020-11-04 11:50:55 +0800

    new add more urls

--- Tgopl/fetchAll/main.go
+++ Tgopl/fetchAll/main.go
@@ -6,38 +6,60 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
 	"time"
 )
 
+func fetchUrl(url string, ch chan<- map[string]string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	result := make(map[string]string)
+	if err != nil {
+		result[url] = fmt.Sprintf("http-get: %v", err)
+		ch <- result
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		result[url] = fmt.Sprintf("while reading:%v %v", url, err)
+		ch <- result
+		return
+	}
+
+	resp.Body.Close()
+	secs := time.Since(start).Seconds()
+	result[url] = fmt.Sprintf("%v %v %v Bytes %v's", url, resp.Status, nbytes, secs)
+	ch <- result
+}
+
 func main() {
 	start := time.Now()
-	ch := make(chan string)
+	ch := make(chan map[string]string)
+	count := 0
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "https://") {
 			url = "https://" + url
 		}
-		go fetch(url, ch)
-	}
-	for range os.Args[1:] {
-		fmt.Println(<-ch)
-	}
-	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
-}
-
-func fetch(url string, ch chan<- string) {
-	start := time.Now()
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
+		fmt.Printf("start fetch %v\n", url)
+		go fetchUrl(url, ch)
+		go fetchUrl(url, ch)
+		go fetchUrl(url, ch)
+		count += 3
 	}
 
-	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	result := make(map[string]string)
+	keys := []string{}
+	for i := 0; i < count; i++ {
+		for k, v := range <-ch {
+			result[k] = v
+			keys = append(keys, k)
+		}
 	}
-	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("%s: %s\n", k, result[k])
+	}
+	fmt.Printf("done, use %v seconds", time.Since(start).Seconds())
 }
