ec7a364a09cc61b7e3c72fd101e85a42f675d2ae
P33king <yunhao.linux@gmail.com>
2020-11-06 14:09:44 +0800

    modified pointertest.go

--- Tgopl/ch2/pointertest.go
+++ Tgopl/ch2/pointertest.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	x := 1
 	p := &x // & memory address
-	fmt.Println(p)
+	fmt.Println(*p)
 	*p = 2 //  * read point var value
 	fmt.Println(x)
+	/*	var x, y int
+
+		fmt.Println(&x == &x, &x == &y, &x == nil) */
 }
