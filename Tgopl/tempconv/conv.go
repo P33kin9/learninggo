b83f01b93be06c1e53b1d7bc839b30812b4eb455
P33king <yunhao.linux@gmail.com>
2020-11-23 16:10:21 +0800

    add file cf and tempconv

---
+++ Tgopl/tempconv/conv.go
@@ -0,0 +1,6 @@
+package tempconv
+
+// CToF converts a Celsius temprature to Fathrentheit.
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
