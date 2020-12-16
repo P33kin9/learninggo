546c1b6d636357fa4733cd7498af980a8d6cb2f7
P33king <yunhao.linux@gmail.com>
2020-12-16 17:29:36 +0800

    breadFirst list findlink3

---
+++ Tgopl/ch5/findlinks3/findlinks.go
@@ -0,0 +1,48 @@
+// Findlinks3 crawls the web, starting with the URLs on the command line.
+package main
+
+import (
+	"cf/ch5/links"
+	"fmt"
+	"log"
+	"os"
+)
+
+//!+breadthFirst
+// breadFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+//!breadthFirst
+
+//!+crawl
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+//!-crawl
+
+//!+main
+func main() {
+	// Crawl the web breadth-first,
+	// starting from the command-line argument.
+	breadFirst(crawl, os.Args[1:])
+}
