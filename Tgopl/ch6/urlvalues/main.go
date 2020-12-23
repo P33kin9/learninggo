9f32d058500b50156c026733db04d363cc6b984b
P33king <yunhao.linux@gmail.com>
2020-12-23 17:10:36 +0800

    The urlvalues command demonstrates a map type with methods.

---
+++ Tgopl/ch6/urlvalues/main.go
@@ -0,0 +1,23 @@
+// The urlvalues  command demonstrates a map type with methods.
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	//!+main
+	m := url.Values{"lang": {"en"}}
+	m.Add("item", "1")
+	m.Add("item", "2")
+
+	fmt.Println(m.Get("lang"))
+	fmt.Println(m.Get("q"))
+	fmt.Println(m.Get("item"))
+	fmt.Println(m["item"])
+
+	m = nil
+	fmt.Println(m.Get("item"))
+	m.Add("item", "3")
+}
