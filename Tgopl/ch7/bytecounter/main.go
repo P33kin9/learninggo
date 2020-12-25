695363c3245bd6f7f730175df5fca983d47d3fde
P33king <yunhao.linux@gmail.com>
2020-12-25 16:30:40 +0800

    Bytecounter demonstrates an implementation of io.Writer that counts bytes.

---
+++ Tgopl/ch7/bytecounter/main.go
@@ -0,0 +1,27 @@
+// Bytecouter demonstrates an implementation of io.Writer that counts bytes.
+package main
+
+import "fmt"
+
+//!+bytecounter
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+//!-bytecounter
+
+func main() {
+	//!+main
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+	//!-main
+}
