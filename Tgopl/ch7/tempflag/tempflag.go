4c18bd8b3d9d5b27e0a98474c7e3a81f01c478af
P33king <yunhao.linux@gmail.com>
2020-12-29 15:40:44 +0800

    Tempflag prints the value of its -temp (temperature) flag.

---
+++ Tgopl/ch7/tempflag/tempflag.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"cf/ch7/tempconv"
+	"flag"
+	"fmt"
+)
+
+//!+
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
