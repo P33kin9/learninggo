0675c6b2f1cb9b26b92d74b9fa4e0eaf7b51cac6
P33king <yunhao.linux@gmail.com>
2021-01-05 15:28:40 +0800

    handle update method

---
+++ Tgopl/ch7/exercise/handle-update.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	db := database{map[string]dollars{"shoes": 50, "socks": 5}, sync.Mutex{}}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/delete", db.delete)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database struct {
+	R     map[string]dollars
+	mutex sync.Mutex
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db.R {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
+
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	p, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		fmt.Fprint(w, "invalid price")
+	} else {
+		db.R[item] = dollars(p)
+		fmt.Fprint(w, "Update price success")
+	}
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	db.mutex.Lock()
+	defer db.mutex.Unlock()
+
+	item := req.URL.Query().Get("item")
+	delete(db.R, item)
+	fmt.Fprint(w, "Success")
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db.R[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
