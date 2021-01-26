ec6e1b5370595f84943e4388eeb83122a40d43af
P33king <yunhao.linux@gmail.com>
2021-01-26 15:43:32 +0800

    mv popcount ch2

---
+++ Tgopl/ch2/popcount/popcount_test.go
@@ -0,0 +1,34 @@
+package popcount_test
+
+import (
+	"cf/popcount"
+	"reflect"
+	"testing"
+)
+
+// --Aternative implementations --
+func assert(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("(expected, actual) = (%v, %v)\n", expected, actual)
+	}
+}
+
+func TestPopCount(t *testing.T) {
+	assert(t, 32, popcount.PopCount(0x123456789ABCDEF))
+}
+
+func TestPopCountByBitClear(t *testing.T) {
+	assert(t, 32, popcount.PopCountByBitClear(0x123456789ABCDEF))
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount(0x123456789ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByBitClear(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountByBitClear(0x123456789ABCDEF)
+	}
+}
