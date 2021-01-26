35468fbabbe06dcbc3b22d8db7e6d72aabd04657
P33king <yunhao.linux@gmail.com>
2021-01-26 19:29:25 +0800

    rm -r ../../../src

--- src/exercise-slices.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/golang/tour/pic"
-import "fmt"
-
-func Pic(dx, dy int) [][]uint8 {
-	a := make([][]uint8, dy)
-	fmt.Println(a)
-}
--- src/go.mod
+++
@@ -1,3 +0,0 @@
-module worktest
-
-go 1.15
