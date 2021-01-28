6defc36f7ee141e1a14d10c4ba7e29d16c5077ab
P33king <yunhao.linux@gmail.com>
2021-01-28 14:15:29 +0800

    fix bug

--- Tgopl/ch7/eval/check.go
+++ Tgopl/ch7/eval/check.go
@@ -35,7 +35,11 @@ func (b binary) Check(vars map[Var]bool) error {
 func (c call) Check(vars map[Var]bool) error {
 	arity, ok := numParams[c.fn]
 	if !ok {
-		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d",
+			c.fn, len(c.args), arity)
 	}
 	for _, arg := range c.args {
 		if err := arg.Check(vars); err != nil {
