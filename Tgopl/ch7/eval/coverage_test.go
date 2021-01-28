3a1eb47d5dea28fd5b669ef940a901e7d4637713
P33king <yunhao.linux@gmail.com>
2021-01-28 14:46:30 +0800

    modify "unknow" = "unknown"

--- Tgopl/ch7/eval/coverage_test.go
+++ Tgopl/ch7/eval/coverage_test.go
@@ -15,7 +15,7 @@ func TestCoverage(t *testing.T) {
 	}{
 		{"x % 2", nil, "unexpected '%'"},
 		{"!true", nil, "unexpected '!'"},
-		{"log(10)", nil, `unknow function "log"`},
+		{"log(10)", nil, `unknown function "log"`},
 		{"sqrt(1, 2)", nil, "call to sqrt has 2 args, want 1"},
 		{"sqrt(A / pi)", Env{"A": 87616, "pi": math.Pi}, "167"},
 		{"pow(x, 3) + pow(y, 3)", Env{"x": 9, "y": 10}, "1729"},
