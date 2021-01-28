fabee1eaa89f505de3ed5051c0cf684efd48fbb4
P33king <yunhao.linux@gmail.com>
2021-01-28 15:38:39 +0800

    modify array performance improve 51%

--- Tgopl/ch11/word2/word.go
+++ Tgopl/ch11/word2/word.go
@@ -9,7 +9,10 @@ import (
 // IsPalindrome reports whether s reads the same forward and backword.
 // Letter case is ignored, as are non-letters.
 func IsPalindrome(s string) bool {
-	var letters []rune
+	//	var letters []rune
+	// BenchmarkIsPalinfrome performance improve 35%.
+
+	letters := make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
