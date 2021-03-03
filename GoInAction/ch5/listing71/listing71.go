279290c12f01b1f9595f18c8654c9a5a9b5e2069
P33king <yunhao.linux@gmail.com>
2021-03-03 17:29:12 +0800

    Sample program to show how unexported fields from an exported  struct type can't be accessed directly.

---
+++ GoInAction/ch5/listing71/listing71.go
@@ -0,0 +1,20 @@
+// Sample program to show how unexported fields from an exported
+// struct type can't be accessed directly.
+package main
+
+import (
+	"fmt"
+	"goination/ch5/listing71/entities.go"
+)
+
+// main is the entry point for the application.
+func main() {
+	// Create a value of type User from the entities package.
+	u := entities.User{
+		Name:  "Bill",
+		email: "bill@email.com",
+	}
+	// ./listing71.go:15:3: cannot refer to unexported field 'email' in
+	// struct literal of type entities.User
+	fmt.Printf("User: %v\n", u)
+}
