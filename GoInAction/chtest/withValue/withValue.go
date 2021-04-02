28b7669baa6ef607808714630bbc910cecd16e1f
P33king <yunhao.linux@gmail.com>
2021-04-02 10:34:41 +0800

    The withValue of context.Context

---
+++ GoInAction/chtest/withValue/withValue.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+var key string = "name"
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	valueCtx := context.WithValue(ctx, key, "【监控1】")
+	go watch(valueCtx)
+	time.Sleep(10 * time.Second)
+	fmt.Println("completed, notification monitor stopped")
+	cancel()
+
+	time.Sleep(5 * time.Second)
+}
+
+func watch(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(ctx.Value(key), "monitor exited, stopping...")
+			return
+		default:
+			fmt.Println(ctx.Value(key), "goroutine is monitoring...")
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
