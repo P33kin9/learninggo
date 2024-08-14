a3aac9944356fb162acadcfec957ce1dac68deab
P33king <yunhao.linux@gmail.com>
2024-08-14 11:34:06 +0800

    add datas

--- Tgopl/exampleGin/readerdata.go
+++ Tgopl/exampleGin/readerdata.go
@@ -5,13 +5,25 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-func main()  {
+
+func main() {
 	router := gin.Default()
-	router.GET("/someDataFromReader",func(ctx *gin.Context) {
-		response, err := http.Get("https://raw.githubusercontent.com/gin-
-gonic/logo/master/color.png")
-if err != nil || response. {
-	
-}
+	router.GET("/someDataFromReader", func(ctx *gin.Context) {
+		response, err := http.Get("https://raw.githubusercontent.com/gin-gonic/logo/master/color.png")
+		if err != nil || response.StatusCode != http.StatusOK {
+			ctx.Status(http.StatusServiceUnavailable)
+			return
+		}
+
+		reader := response.Body
+		contentLength := response.ContentLength
+		contentType := response.Header.Get("Content-Type")
+
+		extraHeaders := map[string]string{
+			"Content-Dispatch": `attachment; filename=gopher.png`,
+		}
+
+		ctx.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
 	})
+	router.Run(":8080")
 }
