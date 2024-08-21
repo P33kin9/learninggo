0b18d01ad5c0b6c8348005ab6f68cba6c4f70e70
P33king <yunhao.linux@gmail.com>
2024-08-21 16:14:53 +0800

    create custom logf ile

--- Tgopl/exampleGin/log-custom/main.go
+++ Tgopl/exampleGin/log-custom/main.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.Printf("endpoint %v %v %v %v", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+	r.POST("/foo", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "foo")
+	})
+
+	r.GET("/bar", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "bar")
+	})
+
+	r.GET("/status", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "ok")
+	})
+	r.Run()
+}
