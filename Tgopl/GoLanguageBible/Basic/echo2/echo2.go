153115f0ffb9753411c146e008431c6207ef1713
P33king <yunhao.linux@gmail.com>
2024-08-23 17:34:59 +0800

    add exercise 1.3

--- Tgopl/GoLanguageBible/Basic/echo2/echo2.go
+++ Tgopl/GoLanguageBible/Basic/echo2/echo2.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
+// exercise 1.3
+// add time.Now() and time.Since(start).Seconds()
 func main() {
+	start := time.Now()
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+	fmt.Println(time.Since(start).Microseconds())
 }
+
 // exercise 1.2
 // // func main(){
 // 	for i, arg := range os.Args[1:]{
--- Tgopl/GoLanguageBible/Basic/echo3/echo3.go
+++ Tgopl/GoLanguageBible/Basic/echo3/echo3.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
+// exercise 1.3
+// add time.Now() and time.Since(start).Seconds()
 func main() {
+	start := time.Now()
 	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(time.Since(start).Microseconds())
 }
